d08be50d30be62ba6765053649006579ea925704
williamkeane112 <williamkeanekeane@gmai.com>
2026-08-11 13:30:51 +0700

    save before pull

--- app/Http/Controllers/RakController.php
+++ app/Http/Controllers/RakController.php
@@ -23,7 +23,7 @@ class RakController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'rak_kode' => 'required|string|max:255|unique:racks,rak_kode',
+            'rak_kode' => 'required|string|max:255|unique:raks,rak_kode',
             'location' => 'required|string|max:255',
         ]);
 
